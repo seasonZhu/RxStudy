ba2c5c083d40e2a2f3be94bee697f1f095689006
zhujilong <zhujilong2007@sina.com>
2019-01-29 14:30:37 +0800

    RxSwift学习开始 集成基本环境

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxStudy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxStudy
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+end
---
+++ RxStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxStudy
+//
+//  Created by season on 2019/1/29.
+//  Copyright © 2019 season. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
