c8d1c1c184c205ae8531a3840bfb9f5f8038cbad
zhujilong <zhujilong2007@sina.com>
2019-02-02 16:39:39 +0800

    提交代码 涉及系统定位的确实有问题呀

--- RxStudy/AppDelegate.swift
+++ RxStudy/AppDelegate.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let locationManager = CLLocationManager()
+    
+    var currLocation: CLLocation!
+    
+    var lat: Double!
+    
+    var long: Double!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        loadLocation()
         return true
     }
 
@@ -42,5 +51,85 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    //打开定位
+    func loadLocation() {
+        
+        locationManager.delegate = self
+        //定位方式
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //iOS8.0以上才可以使用
+        if(UIDevice.current.systemVersion >= "8.0"){
+            //始终允许访问位置信息
+            locationManager.requestAlwaysAuthorization()
+            //使用应用程序期间允许访问位置数据
+            locationManager.requestWhenInUseAuthorization()
+        }
+        //开启定位
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    
+    //获取定位信息
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //取得locations数组的最后一个
+        let location:CLLocation = locations[locations.count-1]
+        currLocation = locations.last!
+        //判断是否为空
+        if(location.horizontalAccuracy > 0){
+            lat = Double(String(format: "%.1f", location.coordinate.latitude))
+            long = Double(String(format: "%.1f", location.coordinate.longitude))
+            print("纬度:\(long!)")
+            print("经度:\(lat!)")
+            LonLatToCity()
+            //停止定位
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
+    
+    //出现错误
+    func locationManager(_ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?) {
+        print(error)
+    }
+    
+    ///将经纬度转换为城市名
+    func LonLatToCity() {
+        let geocoder: CLGeocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(currLocation) { (placemarks, error) -> Void in
+            
+            if(error == nil) {
+                guard let mark = placemarks?.first else {
+                    return
+                }
+                //城市
+                let city = mark.administrativeArea
+                //国家
+                let country = mark.country
+                //国家编码
+                let CountryCode = mark.isoCountryCode
+                //街道位置
+                let subAdministrativeArea = mark.subAdministrativeArea
+                //具体位置
+                let name = mark.name
+                //省
+                let state = mark.locality
+                //区
+                let subLocality = mark.subLocality
+
+            } else {
+                print(error)
+            }
+        }
+        
+        
+    }
+    
 }
 
