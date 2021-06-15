7edb709b4ecb5c3b7e0aecb12785ad30761df2b2
season <zhujilong1987@163.com>
2021-06-15 15:30:35 +0800

    我的页面编写

--- RxStudy/My/Controller/MyController.swift
+++ RxStudy/My/Controller/MyController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 import RxSwift
 import RxCocoa
+import AcknowList
 
 class MyController: BaseTableViewController {
     
-    let logoutDataSource = ["排行榜", "开源框架引用", "登录"]
+    let logoutDataSource: [My] = [.ranking, .openSource, .login]
     
-    let loginDataSource = ["排行榜", "开源框架引用", "我的积分", "我的收藏", "登出"]
+    let loginDataSource: [My] = [.ranking, .openSource, .myCoin, .myCollect, .logout]
     
-    var currentDataSource = BehaviorRelay<[String]>(value: [])
+    var currentDataSource = BehaviorRelay<[My]>(value: [])
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -48,19 +49,53 @@ class MyController: BaseTableViewController {
         tableView.emptyDataSetDelegate = nil
         
         let isLogin = AccountManager.shared.isLogin.value
-        self.currentDataSource.accept(isLogin ? self.loginDataSource : self.logoutDataSource)
+        currentDataSource.accept(isLogin ? loginDataSource : logoutDataSource)
             
-        currentDataSource.asDriver(onErrorJustReturn: [])
-            .drive(tableView.rx.items) { (tableView, row, text) in
+        currentDataSource.asDriver()
+            .drive(tableView.rx.items) { (tableView, row, my) in
                 if let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") {
-                    cell.textLabel?.text = text
+                    cell.textLabel?.text = my.title
                     return cell
                 }else {
                     let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
-                    cell.textLabel?.text = text
+                    cell.textLabel?.text = my.title
                     return cell
                 }
             }
             .disposed(by: rx.disposeBag)
+        
+        
+        tableView.rx.itemSelected
+            .bind { [weak self] (indexPath) in
+                self?.tableView.deselectRow(at: indexPath, animated: false)
+                let my = self?.currentDataSource.value[indexPath.row]
+                guard let strongMy = my else { return }
+                
+                switch strongMy {
+                case .logout:
+                    break
+                case .openSource:
+                    self?.navigationController?.pushViewController(AcknowListViewController(), animated: true)
+                default:
+                    guard let vc = self?.creatInstance(by: strongMy.path) as? UIViewController else {
+                        return
+                    }
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+            .disposed(by: rx.disposeBag)
+    }
+}
+
+extension MyController {
+    public func creatInstance<T: NSObject>(by className: String) -> T? {
+        guard let nameSpace = Bundle.main.infoDictionary?["CFBundleExecutable"] as? String else {
+            return nil
+        }
+        
+        guard let `class` = NSClassFromString(nameSpace + "." + className), let typeClass = `class` as? T.Type else {
+            return nil
+        }
+        return typeClass.init()
     }
 }
---
+++ RxStudy/My/Enum/My.swift
@@ -0,0 +1,62 @@
+//
+//  MyEnum.swift
+//  RxStudy
+//
+//  Created by season on 2021/6/15.
+//  Copyright © 2021 season. All rights reserved.
+//
+
+import UIKit
+
+enum My {
+    case ranking
+    case openSource
+    case myCoin
+    case myCollect
+    case login
+    case logout
+}
+
+extension My {
+    var title: String {
+        let string: String
+        switch self {
+        case .ranking:
+            string = "排名"
+        case .openSource:
+            string = "开源框架引用"
+        case .myCoin:
+            string = "我的积分"
+        case .myCollect:
+            string = "我的收藏"
+        case .login:
+            string = "登录"
+        case .logout:
+            string = "登出"
+        }
+        return string
+    }
+    
+    var imageName: String? {
+        return nil
+    }
+        
+    var path: String {
+        let string: String
+        switch self {
+        case .ranking:
+            string = "CoinRankListController"
+        case .openSource:
+            string = "AcknowListViewController"
+        case .myCoin:
+            string = "MyCoinController"
+        case .myCollect:
+            string = "MyCollectionController"
+        case .login:
+            string = "LoginController"
+        case .logout:
+            string = "logout"
+        }
+        return string
+    }
+}
