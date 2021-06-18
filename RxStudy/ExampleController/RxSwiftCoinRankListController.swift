9513d6a7510bfaece3360908bc4ce058c0f7792b
season <zhujilong1987@163.com>
2021-06-18 11:41:37 +0800

    优化上下拉的逻辑，细化注释

--- RxStudy/ExampleController/RxSwiftCoinRankListController.swift
+++ RxStudy/ExampleController/RxSwiftCoinRankListController.swift
@@ -18,16 +18,13 @@ import MJRefresh
 class RxSwiftCoinRankListController: BaseViewController {
     
     /// 懒加载tableView
-    lazy var tableView = UITableView(frame: .zero, style: .plain)
+    private lazy var tableView = UITableView(frame: .zero, style: .plain)
     
     /// 初始化page为1
-    var page: Int = 1
-    
-    /// 单页的最大条数,这个是需要和后台规定好的
-    var onePageMaxSize = 30
+    private var page: Int = 1
     
     /// 既是可监听序列也是观察者的数据源
-    var dataSource: BehaviorRelay<[CoinRank]> = BehaviorRelay(value: [])
+    private var dataSource: BehaviorRelay<[CoinRank]> = BehaviorRelay(value: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +60,7 @@ class RxSwiftCoinRankListController: BaseViewController {
             make.edges.equalTo(self.view)
         }
         
+        /// 数据源驱动
         dataSource
             .asDriver(onErrorJustReturn: [])
             .drive(tableView.rx.items) { (tableView, row, coinRank) in
@@ -83,56 +81,67 @@ class RxSwiftCoinRankListController: BaseViewController {
 }
 
 extension RxSwiftCoinRankListController {
-    func refreshAction() {
+    /// 下拉刷新行为
+    private func refreshAction() {
         resetCurrentPageAndMjFooter()
         getCoinRank(page: page)
     }
     
-    func  loadMoreAction() {
+    /// 上拉加载更多行为
+    private func loadMoreAction() {
         page = page + 1
         getCoinRank(page: page)
     }
     
-    func resetCurrentPageAndMjFooter() {
+    /// 下拉的参数与状态重置行为
+    private func resetCurrentPageAndMjFooter() {
         page = 1
         self.tableView.mj_footer?.isHidden = false
         self.tableView.mj_footer?.resetNoMoreData()
     }
     
-    func getCoinRank(page: Int) {
+    /// 网络请求
+    private func getCoinRank(page: Int) {
         myProvider.rx.request(MyService.coinRank(page))
+            /// 转Model
             .map(BaseModel<Page<CoinRank>>.self)
+            /// 由于需要使用Page,所以return到$0.data这一层,而不是$0.data.datas
             .map{ $0.data }
+            /// 解包
             .compactMap { $0 }
+            /// 转换操作
             .asObservable()
             .asSingle()
+            /// 订阅
             .subscribe { event in
                 
-                self.tableView.mj_header?.endRefreshing()
+                /// 订阅事件
+                /// 通过page的值判断是下拉还是上拉(可以用枚举),不管成功还是失败都结束刷新状态
+                page == 1 ? self.tableView.mj_header?.endRefreshing() : self.tableView.mj_footer?.endRefreshing()
                 
                 switch event {
                 case .success(let pageModel):
-                    guard let datas = pageModel.datas else {
-                        return
+                    /// 解包数据
+                    if let datas = pageModel.datas {
+                        /// 通过page的值判断是下拉还是上拉,做数据处理,这里为了方便写注释,没有使用三目运算符
+                        if page == 1 {
+                            /// 下拉做赋值运算
+                            self.dataSource.accept(datas)
+                        }else {
+                            /// 上拉做合并运算
+                            self.dataSource.accept(self.dataSource.value + datas)
+                        }
                     }
-                    if page == 1 {
-                        self.dataSource.accept(datas)
-                        if datas.count < self.onePageMaxSize {
-                            self.tableView.mj_footer?.endRefreshingWithNoMoreData()
-                        }
-                    }else {
-                        self.dataSource.accept(self.dataSource.value + datas)
-                        guard let curPage = pageModel.curPage, let pageCount = pageModel.pageCount else {
-                            return
-                        }
-                        
+                    
+                    /// 解包curPage与pageCount
+                    if let curPage = pageModel.curPage, let pageCount = pageModel.pageCount  {
+                        /// 如果发现它们相等,说明是最后一个,改变foot而状态
                         if curPage == pageCount {
                             self.tableView.mj_footer?.endRefreshingWithNoMoreData()
-                        } else {
-                            self.tableView.mj_footer?.endRefreshing()
                         }
                     }
                 case .error(_):
+                    /// error占时不做处理
                     break
                 }
             }.disposed(by: rx.disposeBag)
