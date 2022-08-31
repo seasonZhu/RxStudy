afc7ae6b6ee1bef16b7bb4a6fee52351d9b50a06
season <zhujilong1987@163.com>
2022-08-31 15:30:26 +0800

    添加注释，优化过期方法

--- RxStudy/Extension/MJRefresh+RxSwift/MJRefresh+RxSwift.swift
+++ RxStudy/Extension/MJRefresh+RxSwift/MJRefresh+RxSwift.swift
@@ -79,7 +79,7 @@ extension Reactive where Base: MJRefreshComponent {
                 observer.onNext(state)
             }
             return target
-        }.takeUntil(deallocated)
+        }.take(until: deallocated)
         
         let bindingObserver = Binder<MJRefreshState>(base) { (component, state) in
             component.state = state
--- RxStudy/Header.swift
+++ RxStudy/Header.swift
@@ -9,6 +9,7 @@
 /// 尝试把所以的第三方库通过一个.swift文件引用后,希望其他文件就可以不用引用了
 /// 但是这个是无法实现的
 /// 另外,在新版的Xcode中,有些OC库,即时不桥接也可以直接在Swift中import,下面的import MJRefresh就是例子
+/// 通过观察,不需要使用桥接,是Pods/Products/MJRefresh.framework/Modules/module.modulemap的配置
 /// 但是,需要注意的是,有些库还是必须要桥接的,比如支付宝的SDK\微信的SDK等,所以并不是完全可以去掉Bridging文件的,已在其他工程中验证
 
 import AcknowList
