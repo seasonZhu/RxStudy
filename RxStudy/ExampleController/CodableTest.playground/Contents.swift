e6b51b7bfb3d80f2cbaab4456bb587df439cb970
season <zhujilong1987@163.com>
2022-09-09 11:45:53 +0800

    数字四舍五入的例子

--- RxStudy/ExampleController/CodableTest.playground/Contents.swift
+++ RxStudy/ExampleController/CodableTest.playground/Contents.swift
@@ -141,3 +141,21 @@ print(someData)
 print(any)
 
 print(someString)
+
+
+let formatter = NumberFormatter()
+formatter.numberStyle = .decimal
+formatter.maximumFractionDigits = 1
+
+let numbers = [1.2, 1.22, 1.25, 1.27, -1.25, 0.03]
+let modes: [NumberFormatter.RoundingMode] = [.halfUp]
+    //[.ceiling, .floor, .up, .down, .halfUp, .halfDown, .halfEven]
+
+for mode in modes {
+    formatter.roundingMode = mode
+    
+    for number in numbers {
+        let some = formatter.string(for: number)
+        print(some!)
+    }
+}
