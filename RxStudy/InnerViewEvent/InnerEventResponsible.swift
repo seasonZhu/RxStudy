112ffbcc1044cc401806a68d96111e24ea46f4ba
season <zhujilong1987@163.com>
2024-01-11 15:25:36 +0800

    优化代码与注释

--- RxStudy/InnerViewEvent/InnerEventResponsible.swift
+++ RxStudy/InnerViewEvent/InnerEventResponsible.swift
@@ -10,6 +10,7 @@ import Foundation
 
 protocol InnerEventConvertible {}
 
+/// 响应子View的外层View去实现这个具体方法
 protocol InnerEventResponsible: UIResponder {
     func innerEventHandle(event: any InnerEventConvertible)
 }
@@ -18,10 +19,11 @@ protocol BubbleEventProtocol {
     func bubbleEvent(_ event: any InnerEventConvertible)
 }
 
+/// 冒泡方法在内层子View中实现
 extension UIView: BubbleEventProtocol {
     /// 一个沿着响应链向上传递事件的方法，bubble=冒泡
     func bubbleEvent(_ eventType: any InnerEventConvertible) {
-        var nextRespnder = self.next
+        var nextRespnder = next
         while nextRespnder != nil {
             if let savior = nextRespnder as? InnerEventResponsible {
                 savior.innerEventHandle(event: eventType)
@@ -32,10 +34,11 @@ extension UIView: BubbleEventProtocol {
      }
 }
 
+/// 因为存在vc里面添加一个addChildController的情况,所以这里还是实现了
 extension UIViewController: BubbleEventProtocol {
     /// 一个沿着响应链向上传递事件的方法，bubble=冒泡
     func bubbleEvent(_ eventType: any InnerEventConvertible) {
-        var nextRespnder = self.next
+        var nextRespnder = next
         while nextRespnder != nil {
             if let savior = nextRespnder as? InnerEventResponsible {
                 savior.innerEventHandle(event: eventType)
