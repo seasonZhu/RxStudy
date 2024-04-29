65340abf84bf4881202225f4f4c8e8fae63abf58
season <zhujilong1987@163.com>
2024-04-29 16:56:52 +0800

    AsyncRelay的封装

--- RxStudy/Account/Controller/LoginController.swift
+++ RxStudy/Account/Controller/LoginController.swift
@@ -100,8 +100,7 @@ class LoginController: AccountBaseController {
         actionButton.rx.tap
             /// 使用withUnretained,可以很优雅的解决[weak self]的问题
             .withUnretained(self)
-            .subscribe(onNext: { owner, action in
-            
+            .subscribe(onNext: { _, _ in
             
         }).disposed(by: rx.disposeBag)
     }
