a057c563afedf32e5d192c68b7a05230c6a27b74
season <zhujilong1987@163.com>
2025-01-24 10:49:46 +0800

    引入了一个CustomerTabBar，优化了源码的写法，我觉得对于后面多业务需求可能会用的上

---
+++ RxStudy/CustomTabBarExample/CustomExtensions.swift
@@ -0,0 +1,57 @@
+//
+//  Extensions.swift
+//  CustomTabBarExample
+//
+//  Created by Jędrzej Chołuj on 18/12/2021.
+//
+
+import UIKit
+
+extension UILabel {
+    func configureWith(_ text: String,
+                       color: UIColor,
+                       alignment: NSTextAlignment,
+                       size: CGFloat,
+                       weight: UIFont.Weight = .regular) {
+        self.font = .systemFont(ofSize: size, weight: weight)
+        self.text = text
+        self.textColor = color
+        self.textAlignment = alignment
+    }
+}
+
+extension UIView {
+    func setupCornerRadius(_ cornerRadius: CGFloat = 0, maskedCorners: CACornerMask? = nil) {
+        layer.cornerRadius = cornerRadius
+        if let corners = maskedCorners {
+            layer.maskedCorners = corners
+        }
+    }
+    
+    func animateClick(completion: @escaping () -> Void) {
+        UIView.animate(withDuration: 0.15) {
+            self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.15) {
+                self.transform = CGAffineTransform.identity
+            } completion: { _ in completion() }
+        }
+    }
+    
+    func addSubviews(_ views: UIView...) {
+        views.forEach { addSubview($0) }
+    }
+    
+    func addShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.4
+        layer.shadowRadius = 7
+    }
+}
+
+extension UIStackView {
+    func addArrangedSubviews(_ subviews: [UIView]) {
+        subviews.forEach { addArrangedSubview($0) }
+    }
+}
---
+++ RxStudy/CustomTabBarExample/CustomTabBar.swift
@@ -0,0 +1,72 @@
+//
+//  CustomTabBar.swift
+//  CustomTabBarExample
+//
+//  Created by Jędrzej Chołuj on 18/12/2021.
+//
+
+import UIKit
+
+import NSObject_Rx
+import RxSwift
+import RxCocoa
+import RxGesture
+
+final class CustomTabBar: UIStackView {
+    
+    private lazy var customItemViews = CustomTabItem.allCases.map { $0.customItemView }
+    
+    let itemTappedRelay = PublishRelay<Int>()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupHierarchy()
+        setupProperties()
+        bind()
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+        selectItem(index: 0)
+    }
+    
+    deinit {
+        deinitDDLog()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupHierarchy() {
+        addArrangedSubviews(customItemViews)
+    }
+    
+    private func setupProperties() {
+        distribution = .fillEqually
+        alignment = .center
+        
+        backgroundColor = .systemIndigo
+        setupCornerRadius(30)
+        
+        customItemViews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.clipsToBounds = true
+        }
+    }
+    
+    private func selectItem(index: Int) {
+        customItemViews.forEach { $0.isSelected = $0.item.index == index }
+        itemTappedRelay.accept(index)
+    }
+    
+    // MARK: - Bindings
+    
+    private func bind() {
+        customItemViews.forEach { itemView in
+            itemView.rx.tapGesture().when(.recognized).bind { [weak self] _ in
+                self?.selectItem(index: itemView.item.index)
+            }.disposed(by: rx.disposeBag)
+        }
+    }
+}
